ba34b0f2dc74d940b27a76e31d7c3e9fbafbe0f3
CristianAndreoli94 <125611587+CristianAndreoli94@users.noreply.github.com>
2024-04-27 20:24:45 +0200

    Update CannyEdgeDetection.py

--- CannyEdgeDetection.py
+++ CannyEdgeDetection.py
@@ -305,6 +305,8 @@ cv2.imwrite("edges.jpg", edges)
 
 # Plotting both images in one graph
 fig, axs = plt.subplots(3, 3, figsize=(15, 15))
+# Adapt the window size to fully display the plot
+plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05, hspace=0.2, wspace=0.2)
 
 axs[0][0].imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
 axs[0][0].set_title('Original Image')
